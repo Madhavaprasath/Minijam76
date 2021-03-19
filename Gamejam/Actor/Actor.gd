a298f037dc343890c2e3ac89c6c040917f3ce434
Madhava Prasath <Madhavprasath088@gmail.com>
2021-03-20 02:16:43 +0530

    Player physic

--- Gamejam/Actor/Actor.gd
+++ Gamejam/Actor/Actor.gd
@@ -1,16 +1,21 @@
 extends KinematicBody2D
 
-var speed=250
-var jump_speed=-500
+const speed=250
+const jump_speed=-500
+const gravity=900
+
 var states={}
 var current_state
 var velocity=Vector2()
 
+
 func _physics_process(delta):
 	apply_movement(delta)
-	var transition=match_state(delta)
-	if transition !=null:
-		animation(transition)
+	var state=match_state(delta)
+	if state !=null:
+		current_state=state
+		print(state)
+		animation(state)
 
 func apply_movement(delta):
 	pass
@@ -18,5 +23,5 @@ func apply_movement(delta):
 func match_state(delta):
 	return null
 
-func animation(transition):
+func animation(state):
 	pass 
--- Gamejam/Actor/Player/Player.gd
+++ Gamejam/Actor/Player/Player.gd
@@ -1,17 +1,69 @@
 extends "res://Actor/Actor.gd"
 
 
+class jump:
+	var jumping=false
+	var can_jump=true
+	var can_smash=true
+class smash:
+	var smashing=false
+class moving:
+	var direction=1
+onready var jump_state=jump.new()
+onready var smash_state=smash.new()
+onready var moving_state=moving.new()
 func _init():
-	states={1:"moving",
+	states={1:"Moving",
 			2:"Jump",
-			3:"smash"}
+			3:"Smash",
+			4:"Fall"}
+
+func _ready():
+	current_state=states[1]
 
 func apply_movement(delta):
-	velocity.x=speed
+	Move(delta)
+	apply_gravity(delta)
+	if current_state in ["Jump"]:
+		if is_on_floor():
+			jump()
+		elif !is_on_floor():
+			jump_state.jumping=false
 	velocity=move_and_slide(velocity,Vector2.UP)
-
 func match_state(delta):
+	match current_state:
+		"Moving":
+			if jump_state.jumping:
+				return states[2]
+		"Jump":
+			if smash_state.smashing:
+				return states[3]
+			if !jump_state.jumping:
+				return states[4]
+		"Smash":
+			if is_on_floor():
+				return states[1]
+		"Fall":
+			if is_on_floor():
+				return states[1]
 	return null
 
 func animation(transition):
 	pass 
+
+func _unhandled_key_input(event):
+	if event.is_action_pressed("ui_up") && current_state in ["Moving"]:
+		jump_state.jumping=true
+
+func Move(delta):
+	if Input.is_action_pressed("ui_left"):
+		moving_state.direction=-2
+	elif Input.is_action_pressed("ui_right"):
+		moving_state.direction=2
+	else:
+		moving_state.direction=1
+	velocity.x=speed*moving_state.direction
+func jump():
+	velocity.y=jump_speed
+func apply_gravity(delta):
+	velocity.y+=gravity*delta
